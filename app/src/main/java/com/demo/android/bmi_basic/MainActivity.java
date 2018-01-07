6110a4d72de0dd8bafda2faa55922bdea4b79ea2
ShawnWang126 <littleshawn@gmail.com>
2018-01-07 16:14:57 +0800

    V1.2_20180107 Dialog & Toast

--- app/src/main/java/com/demo/android/bmi_basic/MainActivity.java
+++ app/src/main/java/com/demo/android/bmi_basic/MainActivity.java
@@ -1,15 +1,20 @@
 package com.demo.android.bmi_basic;
 
 
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     //Define parameters
     private Button button;
@@ -17,8 +22,6 @@ public class MainActivity extends AppCompatActivity{
     private EditText fieldweight;
     private TextView result;
     private TextView fieldsuggest;
-
-    //Test Commit //Test Commit //Test Commit //Test Commit //Test Commit
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +31,10 @@ public class MainActivity extends AppCompatActivity{
         findViewSetting();
         setListeners();
 //        button.setOnClickListener(listener);
+        openOptionDialog();
     }
 
-    View.OnClickListener listener = new View.OnClickListener(){
+    View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 //            System.out.println("使用匿名的Listener");
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity{
             double BMI = 0;//Call BMI calculate function
             try {
                 BMI = calBMI();
+                Toast.makeText(MainActivity.this,"Toast Calculate",Toast.LENGTH_LONG).show();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -46,38 +51,56 @@ public class MainActivity extends AppCompatActivity{
         }
     };
 
+    DialogInterface.OnClickListener ConfirmListener = new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialogInterface, int i) {
+//            System.out.print("Show Message");
+        }
+    };
+
+    //Add Dialog
+    private void openOptionDialog() {
+        AlertDialog.Builder DialogBuiler = new AlertDialog.Builder(MainActivity.this);
+        DialogBuiler.setTitle(R.string.dialog_title);//Set Dialog Title
+        DialogBuiler.setMessage(R.string.dialog_message);//Set Dialog Message
+        DialogBuiler.setPositiveButton(R.string.dialog_button_confirm, ConfirmListener);//Set Dialog Confirm Button
+        DialogBuiler.setNegativeButton(R.string.dialog_button_cancel, null);//Set Dialog Cancel Button
+    }
+
+
     //Setting findView
-    private void findViewSetting(){
-        button = (Button)findViewById(R.id.submit);
+    private void findViewSetting() {
+        button = (Button) findViewById(R.id.submit);
         fieldheight = (EditText) findViewById(R.id.height);
         fieldweight = (EditText) findViewById(R.id.weight);
-        result = (TextView)findViewById(R.id.result);
-        fieldsuggest = (TextView)findViewById(R.id.suggest);
+        result = (TextView) findViewById(R.id.result);
+        fieldsuggest = (TextView) findViewById(R.id.suggest);
     }
 
     //Setting Listeners
-    private void setListeners(){button.setOnClickListener(listener);}
+    private void setListeners() {
+        button.setOnClickListener(listener);
+    }
 
-    private double calBMI(){
+    private double calBMI() {
 //        EditText fieldheight = (EditText) findViewById(R.id.height);//Find EditText to find height text
 //        EditText fieldweight = (EditText) findViewById(R.id.weight);//Find EditText to find weight text
-        double height = Double.parseDouble(fieldheight.getText()+"")/100;//Change text height type to double
-        double weight = Double.parseDouble(fieldweight.getText()+"");//Change text weight type to double
-        double BMI = weight/(height*height);
+        double height = Double.parseDouble(fieldheight.getText() + "") / 100;//Change text height type to double
+        double weight = Double.parseDouble(fieldweight.getText() + "");//Change text weight type to double
+        double BMI = weight / (height * height);
         return BMI;
     }
 
-    private void showResult(double BMI){
+    private void showResult(double BMI) {
         DecimalFormat nf = new DecimalFormat("0.00");//Define nf after point 2 (xxx.oo)
 //        TextView result = (TextView)findViewById(R.id.result);//Find TextView to find result
-        result.setText("Your BMI is : "+nf.format(BMI));
+        result.setText("Your BMI is : " + nf.format(BMI));
 //        TextView fieldsuggest = (TextView)findViewById(R.id.suggest);
 
-        if (BMI > 25){
+        if (BMI > 25) {
             fieldsuggest.setText(R.string.advice_heavy);
-        }else if(BMI < 20){
+        } else if (BMI < 20) {
             fieldsuggest.setText(R.string.advice_light);
-        }else{
+        } else {
             fieldsuggest.setText(R.string.advice_average);
         }
     }
